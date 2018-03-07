1e53e2c3cdad9630be69d5da47b9e7c0372f0aa7
swarnim-97 <swarnimpradhan1997@gmail.com>
2018-03-07 14:42:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title:'Article One',
+    heading: 'Article One',
+    content:`
+    <p>
+                After Vladimir Lenin's statue was brought down at Belonia College Square in Agartala following the BJP's victory in the Tripura Assembly election, there have been reports of vandalism on statues across the country.
+                A post in BJP's National Secretary H.Raja's Facebook Page against Periyar statues in Tamil Nadu triggered Statewide protests. Mr. Raja subsequently removed the post and expressed regret, but the issue refused to die down.
+            </p>`
+    
+};
+
+function createtemplate (data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class=wholebody>
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <h2>${heading}</h2>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +57,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(createtemplate(content));
 });
 
 app.get('/ui/madi.png', function (req, res) {
