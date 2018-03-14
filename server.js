28ffdc7e13c731a13afee54e5cb5a5cb96bbb7e3
swarnim-97 <swarnimpradhan1997@gmail.com>
2018-03-15 02:54:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,6 +56,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/article-one', function(req,res){
    res.send(createtemplate(content));
 });
